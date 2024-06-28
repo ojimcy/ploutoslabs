1976bf264b070484c20c8157ecbeaa995dfae67f
Emmanuel <ojimcy247@gmail.com>
2024-06-28 16:01:55 +0100

    updated

--- src/components/navs/MobileNav.js
+++ src/components/navs/MobileNav.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Nav } from 'reactstrap';
-import { FaInstagram, FaLinkedin, FaTelegram, FaTwitter } from 'react-icons/fa';
 import PropTypes from 'prop-types';
 
 import './MobileNav.css';
 
-function MobileMenu({ isOpen, toggleMobileMenu, scrollToSection }) {
+function MobileMenu({ isOpen, toggleMobileMenu }) {
   const closeMenu = () => {
     if (isOpen) {
       toggleMobileMenu(); // Close the mobile menu
     }
-  };
-
-  const handleScrollToSection = (sectionId) => {
-    scrollToSection(sectionId);
-    closeMenu(); // Close the mobile menu after scrolling
   };
 
   return (
@@ -25,69 +19,6 @@ function MobileMenu({ isOpen, toggleMobileMenu, scrollToSection }) {
         <Link className="nav-link" to="/" onClick={closeMenu}>
           Home
         </Link>
-        <Link
-          className="nav-link"
-          to="/#tokenomics"
-          onClick={() => handleScrollToSection('tokenomics')}
-        >
-          Tokenomics
-        </Link>
-        <Link
-          className="nav-link"
-          to="/#nft"
-          onClick={() => handleScrollToSection('nft')}
-        >
-          Nft
-        </Link>
-        <Link
-          className="nav-link"
-          to="/#gaming"
-          onClick={() => handleScrollToSection('gaming')}
-        >
-          Gaming
-        </Link>
-        <Link
-          className="nav-link"
-          to="/#roadmap"
-          onClick={() => handleScrollToSection('roadmap')}
-        >
-          Roadmap
-        </Link>
-        <Link
-          className="nav-link"
-          to="/#airdrop"
-          onClick={() => handleScrollToSection('airdrop')}
-        >
-          Airdrop
-        </Link>
-
-        <Link className="nav-link" to="/auth/login">
-          Login
-        </Link>
-        <Link
-          className="nav-link "
-          to="https://ploutoslabs.gitbook.io/ploutos-white-paper"
-        >
-          Whitepaper
-        </Link>
-
-        {/* Social Icons */}
-        <div className="navbar-bottom">
-          <div className="social-icons">
-            <Link to="#" target="_blank" rel="noopener noreferrer">
-              <FaTelegram />
-            </Link>
-            <Link to="#" target="_blank" rel="noopener noreferrer">
-              <FaTwitter />
-            </Link>
-            <Link to="#" target="_blank" rel="noopener noreferrer">
-              <FaInstagram />
-            </Link>
-            <Link to="#" target="_blank" rel="noopener noreferrer">
-              <FaLinkedin />
-            </Link>
-          </div>
-        </div>
       </Nav>
     </div>
   );
