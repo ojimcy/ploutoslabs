fbbbe417d065bf41f5b11df517532f7c09f79be5
Emmanuel <ojimcy247@gmail.com>
2024-08-16 11:53:49 +0100

    deposit modal

--- src/components/common/modal/GameDepositModal.js
+++ src/components/common/modal/GameDepositModal.js
@@ -1,21 +1,41 @@
-import React  from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import { Modal, ModalHeader, ModalBody } from 'reactstrap';
+import { Modal, ModalHeader, ModalBody, Button } from 'reactstrap';
 import './modal.css';
-import { useNavigate } from 'react-router-dom';
 
 function GameDepositModal({ isOpen, toggle }) {
-  const navigate = useNavigate();
+  const [depositAddress, setDepositAddress] = useState('');
+
+  useEffect(() => {
+    const fetchDepositAddress = async () => {
+      // Replace with actual API call to fetch deposit address
+      const address = '0x1234567890abcdef1234567890abcdef12345678';
+      setDepositAddress(address);
+    };
+
+    fetchDepositAddress();
+  }, []);
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(depositAddress);
+    alert('Deposit address copied to clipboard!');
+  };
 
   return (
     <Modal isOpen={isOpen} toggle={toggle} className="main-modal" fade={false}>
       <ModalHeader toggle={toggle}>Create Game</ModalHeader>
       <ModalBody>
-        <div className='game-deposit'>
+        <div className="game-deposit">
+          <p>Your deposit address is:</p>
+          <div className="deposit-address">
+            <code>{depositAddress}</code>
+          </div>
 
+          <Button color="primary" onClick={copyToClipboard}>
+            Copy Address
+          </Button>
         </div>
       </ModalBody>
-
     </Modal>
   );
 }
