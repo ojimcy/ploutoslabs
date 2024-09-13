58b32904d628aa4b16b562ed057551dc966dc877
Ademu Anthony <ademuanthony@gmail.com>
2024-09-13 12:59:45 +0100

    send eth

--- src/components/common/modal/GameDepositModal.js
+++ src/components/common/modal/GameDepositModal.js
@@ -25,7 +25,7 @@ function GameDepositModal({ isOpen, toggle }) {
       <ModalHeader toggle={toggle}>Create Game</ModalHeader>
       <ModalBody>
         <div className="game-deposit">
-          <p>Send USDC (base network) to the following address:</p>
+          <p>Send ETH (base network) to the following address:</p>
           <div className="deposit-address">
             <code>{depositAddress}</code>
           </div>
