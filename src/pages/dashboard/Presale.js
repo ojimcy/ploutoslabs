b5039b2e1b150b4dfb7504ef5bf8325ef60be9a1
ojimcy <ojimcy247@gmail.com>
2025-02-28 17:28:23 +0100

    extend presale

--- src/pages/dashboard/Presale.js
+++ src/pages/dashboard/Presale.js
@@ -72,7 +72,7 @@ function TokenPresale() {
 
   const currentPrice = 138000;
 
-  const presaleEndTime = new Date('2025-02-28T23:59:59Z').getTime();
+  const presaleEndTime = new Date('2025-03-3T23:59:59Z').getTime();
 
   useEffect(() => {
     // Countdown timer setup
